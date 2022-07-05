322bd89ee70d619631ebab99f12b00bea7474b75
Travis <trmail25@gmail.com>
2022-07-05 10:53:09 -0400

    basic functionality

--- src/CookieClicker.java
+++ src/CookieClicker.java
@@ -1,13 +1,60 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CookieClicker {
+	public WebDriver Driver;
+	private WebElement Cookie;
+	private WebElement CookieCounter;
+	
 	public CookieClicker()
 	{
 		System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver/");
 		System.setProperty("webdriver.firefox.bin", "/home/travis/firefox/firefox");
-		WebDriver driver = new FirefoxDriver();
-		driver.get("https://orteil.dashnet.org/cookieclicker/");
+		this.Driver = new FirefoxDriver();
+		this.Driver.get("https://orteil.dashnet.org/cookieclicker/");
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		if (Driver.findElements(By.id("promptContentChangeLanguage")).size() > 0)
+		{
+			Driver.findElement(By.xpath("//div[.='English']")).click();
+		}
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		this.Cookie = Driver.findElement(By.id("bigCookie"));
+		this.CookieCounter = Driver.findElement(By.xpath("//div[@id='cookies']"));
+		
 	}
+	
+	public void clickTheCookie()
+	{
+		this.Cookie.click();
+	}
+	
+	public void clickTheCookie(int numberOfClicks)
+	{
+		for(int i = 0; i < numberOfClicks; i++)
+		{
+			this.Cookie.click();
+		}
+	}
+	
+	public int getCookieCount()
+	{
+		return Integer.parseInt(this.CookieCounter.getAttribute("innerHTML").split(" ")[0]);
+	}
+	
+	
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		CookieClicker CC = new CookieClicker();
+		CC.clickTheCookie(100);
+		System.out.println(CC.getCookieCount());
 
 	}
 
